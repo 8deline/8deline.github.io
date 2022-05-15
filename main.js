e3940848ec71079d0667136468a94458f7517b05
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2022-05-15 22:37:29 +0800

    test offset 100 on mobile

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 800) {
+  if (window.pageYOffset > 100) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
